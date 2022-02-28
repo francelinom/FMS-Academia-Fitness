df33f459c0568e494223903aaa99a54afe362638
francelino <francelino.marcilio@quark.tec.br>
2022-02-28 15:47:18 -0300

    Adicionando o header e footer
    Adicionei um header e levei as informaçoes da navegaçao da pagina
    
    adicionei um footer com meu nome como dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
