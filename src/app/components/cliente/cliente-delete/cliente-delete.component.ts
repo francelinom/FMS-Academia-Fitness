bae21944bbfc686f2f657f1640b8cc82ec8248e4
francelino <francelino.marcilio@quark.tec.br>
2022-03-01 20:36:56 -0300

    Complementando o excluir

--- src/app/components/cliente/cliente-delete/cliente-delete.component.ts
+++ src/app/components/cliente/cliente-delete/cliente-delete.component.ts
@@ -1,6 +1,6 @@
 import { Cliente } from './../cliente.model';
 import { ClienteService } from './../cliente.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -11,16 +11,25 @@ import { Component, OnInit } from '@angular/core';
 export class ClienteDeleteComponent implements OnInit {
   cliente!: Cliente;
 
-  constructor(private router: Router, private clienteService: ClienteService) {}
+  constructor(
+    private router: Router,
+    private clienteService: ClienteService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    const id: any = '1';
+    const id: any = this.route.snapshot.paramMap.get('id');
     this.clienteService.readById(id).subscribe((cliente) => {
       this.cliente = cliente;
     });
   }
 
-  excluirCliente(): void {}
+  excluirCliente(): void {
+    this.clienteService.delete(`${this.cliente.id}`).subscribe(() => {
+      alert('Cliente excluido!');
+      this.router.navigate(['/clientes']);
+    });
+  }
 
   cancelar(): void {
     this.router.navigate(['/clientes']);
