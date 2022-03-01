84071137cdae7b1c2387ecef01bdbf7d14b0e9fb
francelino <francelino.marcilio@quark.tec.br>
2022-03-01 12:36:45 -0300

    Criando metodo de readById e update

--- src/app/components/cliente/cliente.service.ts
+++ src/app/components/cliente/cliente.service.ts
@@ -18,4 +18,14 @@ export class ClienteService {
   list(): Observable<Cliente[]> {
     return this.http.get<Cliente[]>(this.url);
   }
+
+  readById(id: string): Observable<Cliente> {
+    const url = `${this.url}/${id}`;
+    return this.http.get(url);
+  }
+
+  update(cliente: Cliente): Observable<Cliente> {
+    const url = `${this.url}/${cliente.id}`;
+    return this.http.put<Cliente>(url, cliente);
+  }
 }
